ad8d90dec759d75fa9e68b2cdbad840102b8dc3f
TheJmqn <jman4461@gmail.com>
2024-07-25 22:40:31 -0400

    Map restaurants from JSON

--- src/Main.js
+++ src/Main.js
@@ -2,33 +2,37 @@ import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Restaurant from './Restaurant';
 import Login from './Login';
-import RisingRollBanner from './banners/RisingRollBanner.jpg';
-import WestRangeBanner from './banners/WestRangeCafeBanner.jpg';
-import ChicFilABanner from './banners/ChickFilABanner.jpg';
+import Restaurants from './restaurants.json';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/restaurants/chic-fil-a",
-    element: <Restaurant name={"Chic-Fil-A"} img={ChicFilABanner} id={1}/>,
-  },
-  {
-    path: "/restaurants/west-range",
-    element: <Restaurant name={"West Range Café"} img={WestRangeBanner} id={2}/>,
-  },
-  {
-    path: "/restaurants/rising-roll",
-    element: <Restaurant name={"Rising Roll"} img={RisingRollBanner} id={3}/>,
-  },
-]);
+const restaurantRouterArray = () => {
+  const restaurantArray = [
+    {
+      path: "/",
+      element: <Home />,
+      errorElement: <Error />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    }
+  ];
+  Restaurants.forEach(restaurant => {
+    restaurantArray.push(
+      {
+        path: `/restaurants/${restaurant.path}`,
+        element: 
+        <Restaurant 
+          name={restaurant.name} 
+          img={`/banners/${restaurant.banner}`} 
+          id={restaurant.id}
+        />
+      }
+    );
+  });
+  return restaurantArray;
+}
+
+const router = createBrowserRouter(restaurantRouterArray());
 
 function Home() {
   return (
@@ -37,9 +41,15 @@ function Home() {
     <a href={`/login`}>Login/Signup</a>
     <p>Restaurants:</p>
     <ul>
-      <li><a href={`/restaurants/chic-fil-a`}>Chic-Fil-A</a></li>
-      <li><a href={`/restaurants/west-range`}>West Range Café</a></li>
-      <li><a href={`/restaurants/rising-roll`}>Rising Roll</a></li>
+      {Restaurants.map((restaurant) => {
+        return (
+        <li key={restaurant.id}>
+          <a href={`/restaurants/${restaurant.path}`}>
+            {restaurant.name}
+          </a>
+        </li>
+        );
+      })}
     </ul>
   </div>
   );
